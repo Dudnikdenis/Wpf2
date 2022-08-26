eff0b28ab2cc8759c56137d2827ea5a8a1af8ecb
denwee87 <dudnik.denis.87@mail.ru>
2022-08-26 16:27:00 +0500

    Добавили и реализовали команду ClouseAppComand

--- ViewModel/MyWindowViewModel.cs
+++ ViewModel/MyWindowViewModel.cs
@@ -4,6 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Wpf2.Command;
 using Wpf2.Data;
 using Wpf2.Model;
 using Wpf2.ViewModel.Base;
@@ -11,7 +14,8 @@ using Wpf2.ViewModel.Base;
 namespace Wpf2.ViewModel
 {
     class MyWindowViewModel : ViewModelBase
-    {        
+    {
+        #region Текст для вкладки
         private string hello = "Расходы ура";
 
         public string Hello
@@ -24,6 +28,25 @@ namespace Wpf2.ViewModel
                 hello = value;
                 OnPropertyChanged("Hello");
             }
-        }        
+        }
+        #endregion
+
+        #region ClouseAppCommand
+        public ICommand ClouseAppCommand { get; }
+
+        private bool CanClouseAppCommandExecut(object p) => true;
+
+        private void OnClouseAppCommandExecuted (object p )
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        public MyWindowViewModel()
+        {
+            #region Команды
+            ClouseAppCommand = new LiambdaCommand(OnClouseAppCommandExecuted, CanClouseAppCommandExecut);
+            #endregion
+        }
     }
 }
