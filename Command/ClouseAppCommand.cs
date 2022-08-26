9d27ea685aa33e69888d4f6887d994fdafcdce17
denwee87 <dudnik.denis.87@mail.ru>
2022-08-26 16:56:32 +0500

    Создали Класс-команду

---
+++ Command/ClouseAppCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Wpf2.Command.Base;
+
+namespace Wpf2.Command
+{
+    internal class ClouseAppCommand : CommandBase
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
